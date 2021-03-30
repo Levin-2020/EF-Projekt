f47c8eeeb9e0a06740498b011f64c1763f5d2104
Levin-2020 <63237454+Levin-2020@users.noreply.github.com>
2021-03-30 15:35:16 +0200

    aesgdf

---
+++ world_countries_black/convert.py
@@ -0,0 +1,76 @@
+import time
+import os
+
+os.chdir(r'C:\Users\levin\Desktop\EF-Projekt\world_countries_black')
+
+f = open("world.svg", "r")
+data_raw = f.read()
+print("done")
+path_count = data_raw.count("<path")
+print("done2")
+data = []
+ids = {}
+data_dic = {}
+id_start = 2000
+
+for i in range(path_count):
+    s = data_raw.find("<path")
+    e = data_raw.find("</path>")
+    temp = data_raw[s:e+7]
+    data_raw = data_raw[:s] + data_raw[e+8:]
+    data.append(temp)
+
+
+print(data)
+"""
+
+f_string = ""
+
+countries_csv = ""
+svg_csv = ""
+geo_csv = ""
+
+for d in data:
+    c = d.find("class=")
+    if c == -1:
+        c1 = d.find("name=")
+    if c != -1:
+        temp = d[c+7:]
+        name = temp[:temp.find("\"")]
+    else:
+        temp = d[c1+6:]
+        name = temp[:temp.find("\"")]
+
+    if name in ids.keys():
+        d_id = ids[name]
+    else:
+        d_id = id_start
+        id_start += 1
+        ids[name] = d_id
+    
+    d = d[:-10] + f" onclick=\"click({d_id})\" " + d[-10:]
+    f_string += d + "\n"
+    d = d.replace("\n","")
+    if name in data_dic:
+        data_dic[name] += d
+    else:
+        data_dic[name] = d
+
+
+for key in ids.keys():
+    svg_csv += str(ids[key]) + "," + data_dic[key] + "," + f"A country called {key}" + "," + "2" + "," + str((ids[key])) + "\n"
+    geo_csv += str(ids[key]) + "," + f"{key}" + "," + "country" + "\n"
+
+
+ff = open("svg.csv", "w")
+ff.write(svg_csv)
+ff.close()
+
+ff = open("geo_elements.csv", "w")
+ff.write(geo_csv)
+ff.close()
+
+#f_write = open("test.svg", "w")
+#f_write.write(f_string)
+
+"""
